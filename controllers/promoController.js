e89abaaa469a61be58a5f11e9815364f3b87f5eb
Rodriguez <Eddie.Rodriguez@aarons.com>
2018-10-17 17:14:20 -0400

    updating promotions route

---
+++ controllers/promoController.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Promo
+      .find()
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // findByPass: function(req, res) {
+  //   db.Promo
+  //     .findById(req.params.pass)
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
+  create: function(req, res) {
+    db.Promo
+    .deleteMany({})
+      .then(db.Promo.create(req.body))
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  // update: function(req, res) {
+  //   db.Beer
+  //     .findOneAndUpdate({ _id: req.params.id }, req.body)
+  //     .then(dbModel => res.json(dbModel))
+  //     .catch(err => res.status(422).json(err));
+  // },
+  remove: function(req, res) {
+    db.Promo
+      .deleteMany({})
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
